e4dbd545c0a6efffea7e619cd08e21cbbc0437f7
Jayaram Kottimugalur <jayaramraja1987@gmail.com>
2021-11-23 01:05:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku",
+    version="0.0.3",
+    description="ML project",
+    author="Jay",
+    packages=find_packages(),
+    license="MIT"
+)
